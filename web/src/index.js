6cd6f15c34d0c2c1e6084284ac641d26961d0bb1
n-j-m <nick65@gmail.com>
2021-02-10 23:29:53 -0600

    Initial

---
+++ web/src/index.js
@@ -0,0 +1,24 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import netlifyIdentity from 'netlify-identity-widget'
+import ReactDOM from 'react-dom'
+import { FatalErrorBoundary } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+
+import Routes from 'src/Routes'
+
+import './scaffold.css'
+import './index.css'
+
+netlifyIdentity.init()
+
+ReactDOM.render(
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <AuthProvider client={netlifyIdentity} type="netlify">
+      <RedwoodApolloProvider>
+        <Routes />
+      </RedwoodApolloProvider>
+    </AuthProvider>
+  </FatalErrorBoundary>,
+  document.getElementById('redwood-app')
+)
